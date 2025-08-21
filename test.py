bf6a0d4c57e62898fb0b874512c5df087316fb11
4FTSQRL <156530576+4FTSQRL@users.noreply.github.com>
2025-08-21 10:30:11 -0400

    Create test.py
    Creating a test file for the functionality of the game.

---
+++ test.py
@@ -0,0 +1,15 @@
+"""
+Author: 4FTSQRL
+
+Usage: test.py
+
+Description: The test script for the guessing game for testing functionality of the game
+"""
+
+# Import statements
+
+# Main function for testing
+def main():
+    return 0
+
+# 
