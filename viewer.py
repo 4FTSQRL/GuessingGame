e58718c704283f8d6a306ef728baaf241fe546b1
4FTSQRL <156530576+4FTSQRL@users.noreply.github.com>
2025-08-21 10:26:00 -0400

    Create viewer.py
    Created a tkinter gui

---
+++ viewer.py
@@ -0,0 +1,22 @@
+"""
+Author: 4FTSQRL
+
+Usage: python viewer.py
+
+Description: Viewer for the game. Here is where people can interact with the game and submit their guesses.
+"""
+
+# Import Statements
+#Tkinter
+from tkinter import *
+
+# Create the GUI
+root = Tk()
+# Add a title
+root.title("Guess the Bears!")
+
+# Frames
+frame = Frame(root)
+
+# Calling the main loop for Tk
+root.mainloop()
