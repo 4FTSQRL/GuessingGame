decbdf56bbf20ec662118d2f75fd577c03d48791
4FTSQRL <156530576+4FTSQRL@users.noreply.github.com>
2025-08-21 11:27:28 -0400

    Finished the math and started the viewer
    Finished the math.
    
    Started a the viewer with a name dropdown

--- test.py
+++ test.py
@@ -10,6 +10,8 @@ Description: The test script for the guessing game for testing functionality of 
 
 # Main function for testing
 def main():
+    # Call results
+    print(results())
     return 0
 
 # Answer function
@@ -24,9 +26,38 @@ def guesses():
     # Function for the guesses
     # Dictionary for it
     playerGuesses = {
-        "John Doe": 102,
-        "John Deer": 304,
-        "Jane Doe": 293
+        "John Doe": 0,
+        "John Deer": 0,
+        "Jane Doe": 0,
+        "Joe Momma": 0
     }
-    
     return playerGuesses
+
+# Math Function
+def results():
+    # Calculates results
+    # call the other functions
+    playerGuesses = guesses()
+    realnum = answer()
+    
+    # Store difference
+    diff = [90000]
+    i = 1
+    # Store winner
+    winner = ""
+    print (playerGuesses.items())
+    # For loop to loop through all playerGuess values to find the winnner
+    for g in playerGuesses.items():
+        diff.insert(i, abs(g[1] - realnum))
+        if (diff[i] <= diff[i-1]):
+            winner = g[0]
+        # iterate i
+        i += 1
+        
+    # Sort the difference
+    diff.sort()
+    
+    return winner
+# Python incantation
+if __name__ == "__main__":
+    main()
--- viewer.py
+++ viewer.py
@@ -7,16 +7,32 @@ Description: Viewer for the game. Here is where people can interact with the gam
 """
 
 # Import Statements
+import test
 #Tkinter
 from tkinter import *
+from tkinter import ttk
 
 # Create the GUI
 root = Tk()
 # Add a title
-root.title("Guess the Bears!")
+root.title("Guess the Price")
 
 # Frames
-frame = Frame(root)
+frame = ttk.Frame(root)
+frame.grid(row=0,column=0,columnspan=2)
+
+# Get the keys and clean it up by putting it in a list
+keys = (test.guesses()).keys()
+names = []
+i = 0
+for k in keys:
+    names.insert(i, k)
+    i += 1
+    
+# Combo box
+nameCmbx = ttk.Combobox(frame, values=names)
+nameCmbx.set("Select your name")
+nameCmbx.grid(padx=100, pady=10)
 
 # Calling the main loop for Tk
 root.mainloop()
